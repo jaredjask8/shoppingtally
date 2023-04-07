fd4ad2d51a0a42223c033703eb1eeba4421c00ca
Jared Jaskolski <jaredj8@Jareds-MacBook-Pro.local>
2023-04-07 15:44:12 -0400

    add

--- employeeFrontend/src/app/app-routing.module.ts
+++ employeeFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'test', component: EmployeeComponent },
+	{ path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
