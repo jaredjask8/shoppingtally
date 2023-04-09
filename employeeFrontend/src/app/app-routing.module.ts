517fe11658571243716be199094ff66efd5964fc
Jared Jaskolski <jaredj8@Jareds-MBP.lan>
2023-04-09 09:36:02 -0400

    update routes

--- employeeFrontend/src/app/app-routing.module.ts
+++ employeeFrontend/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EmployeeComponent } from './employee/employee.component';
 
 const routes: Routes = [
-	{ path: 'test', component: EmployeeComponent },
+	{ path: 'list', loadChildren: () => import('../list/list.module').then(m => m.ListModule) },
+	{ path: 'employee', loadChildren: () => import('../employee/employee.module').then(m => m.EmployeeModule) },
+	{ path: 'about', loadChildren: () => import('../about/about.module').then(m => m.AboutModule) },
 	{ path: '**', redirectTo: 'home' }
 ];
 
