1ca70de2789ff32931c3d6e6701ad3a9dc01d0f1
Jared <jaredjaskolski@yahoo.com>
2023-09-21 11:38:38 -0400

    cors

--- src/main/java/com/app/shoppingtally/ServletInitializer.java
+++ src/main/java/com/app/shoppingtally/ServletInitializer.java
@@ -1,10 +1,17 @@
 package com.app.shoppingtally;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
@@ -14,14 +21,15 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	}
 	
 	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("PUT", "DELETE",
-                        "GET", "POST");
-            }
-        };
-    }
+	CorsConfigurationSource corsFilter() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+	    configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+	    configuration.setExposedHeaders(Arrays.asList("Authorization", "content-type"));
+	    configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type"));
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
+	}
 
 }
--- src/main/java/com/app/shoppingtally/config/SecurityConfiguration.java
+++ src/main/java/com/app/shoppingtally/config/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf()
-			.disable()
+			.and()
 			.cors()
 			.and()
 			.authorizeHttpRequests()
