289db9873a4d25cd86446ec399bbd96dfde646d7
Jared <jaredjaskolski@yahoo.com>
2023-09-21 11:46:49 -0400

    cors

--- src/main/java/com/app/shoppingtally/ServletInitializer.java
+++ src/main/java/com/app/shoppingtally/ServletInitializer.java
@@ -25,8 +25,6 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	    CorsConfiguration configuration = new CorsConfiguration();
 	    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
 	    configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-	    configuration.setExposedHeaders(Arrays.asList("Authorization", "content-type"));
-	    configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type"));
 	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 	    source.registerCorsConfiguration("/**", configuration);
 	    return source;
