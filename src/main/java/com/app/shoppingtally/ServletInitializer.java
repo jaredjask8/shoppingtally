bc969b94f52e788a5cbcfa405e8a167deace9e77
Jared <jaredjaskolski@yahoo.com>
2023-09-21 10:53:14 -0400

    update CORS

--- src/main/java/com/app/shoppingtally/ServletInitializer.java
+++ src/main/java/com/app/shoppingtally/ServletInitializer.java
@@ -2,6 +2,9 @@ package com.app.shoppingtally;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
@@ -9,5 +12,16 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(ShoppingtallyApplication.class);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("PUT", "DELETE",
+                        "GET", "POST");
+            }
+        };
+    }
 
 }
