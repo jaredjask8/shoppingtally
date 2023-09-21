9cd79734d69ae3d9d111a80e8c0915f1e52b2d79
Jared <jaredjaskolski@yahoo.com>
2023-09-21 15:00:57 -0400

    cors

--- src/main/java/com/app/shoppingtally/config/SecurityConfiguration.java
+++ src/main/java/com/app/shoppingtally/config/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf()
-			.and()
+			.disable()
 			.cors(Customizer.withDefaults())
 			.authorizeHttpRequests()
 			.requestMatchers("/api/v1/auth/**")
