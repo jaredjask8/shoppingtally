d0fc985299760d60e56e7279b3acd542d5b925da
Jared <jaredjaskolski@yahoo.com>
2023-09-21 11:21:26 -0400

    cors

--- src/main/java/com/app/shoppingtally/auth/AuthenticationController.java
+++ src/main/java/com/app/shoppingtally/auth/AuthenticationController.java
@@ -17,7 +17,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthenticationController {
 	private final AuthenticationService service;
 	
