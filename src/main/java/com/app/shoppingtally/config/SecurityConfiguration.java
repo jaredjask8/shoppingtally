fe6bb66d43c9f73257903e304923f0dce0cb07ec
Jared <jaredjaskolski@yahoo.com>
2023-09-21 14:40:03 -0400

    cors

--- src/main/java/com/app/shoppingtally/config/SecurityConfiguration.java
+++ src/main/java/com/app/shoppingtally/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,11 +29,10 @@ public class SecurityConfiguration {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf()
-			.disable()
-			.cors()
 			.and()
+			.cors(Customizer.withDefaults())
 			.authorizeHttpRequests()
-			.requestMatchers("/**")
+			.requestMatchers("/api/v1/auth/**")
 			.permitAll()
 			.anyRequest()
 			.authenticated()
