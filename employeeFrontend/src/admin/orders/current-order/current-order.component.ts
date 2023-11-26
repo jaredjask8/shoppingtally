ba4a920d4d3805d20f23c9e3acd3c7fb7f71cebd
Jared <jaredjaskolski@yahoo.com>
2023-11-26 18:53:56 -0500

    Revert "change socket server for admin"
    This reverts commit 0bfed1f40ffc8b0b26641025c80bbeb3786aa08a.

--- employeeFrontend/src/admin/orders/current-order/current-order.component.ts
+++ employeeFrontend/src/admin/orders/current-order/current-order.component.ts
@@ -26,7 +26,7 @@ import { CurrentOrderUser } from 'src/list/models/CurrentOrderUser';
 })
 export class CurrentOrderComponent implements OnInit,OnDestroy{
   currentOrder:CurrentOrderShopper;
-  serverUrl = 'https://shoppingtally.click/test/shoppingtally-0.0.2-SNAPSHOT/our-websocket'
+  serverUrl = 'http://localhost/test/our-websocket'
   title = 'WebSockets chat';
   stompClient;
   
