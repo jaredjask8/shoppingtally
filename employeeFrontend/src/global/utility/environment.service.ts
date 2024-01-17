905f29bef33ad81c897a037a9db5da5a88bda942
Jared <jaredjaskolski@yahoo.com>
2024-01-17 17:38:43 -0500

    change logout timing

--- employeeFrontend/src/global/utility/environment.service.ts
+++ employeeFrontend/src/global/utility/environment.service.ts
@@ -86,7 +86,7 @@ import { Router } from "@angular/router";
         this.refreshLogin().subscribe(d=>this.setEnvironment(d.token))
         console.log(this.getEnvironment().token)
         this.stopLogoutTimer()
-      },20000) //900000
+      },900000) //900000
     }
 
     startLogoutTimer(){
@@ -104,7 +104,7 @@ import { Router } from "@angular/router";
             this.signOutSnackbar.next(true)
           }
         })
-      },10000) //700000
+      },700000) //700000
     }
 
     stopLogoutTimer(){
