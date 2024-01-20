5d43b8a5392ce6586d89aa8bf8d9864ba999e84f
Jared <jaredjaskolski@yahoo.com>
2024-01-19 22:23:13 -0500

    shorter time for testing

--- employeeFrontend/src/global/utility/environment.service.ts
+++ employeeFrontend/src/global/utility/environment.service.ts
@@ -88,7 +88,7 @@ import { Router } from "@angular/router";
         //after 15 min
         //logout timer starts when refresh happens
         this.refreshLogin().subscribe(d=>this.setToken(d.token))
-      },4 * 60 * 1000) //900000 //2 * 60 * 1000
+      },2 * 60 * 1000) //900000 //2 * 60 * 1000
     }
 
     startLogoutTimer(){
