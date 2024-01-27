74e04a3b8643e2353780f883e93b36dbaad4beaf
Jared <jaredjaskolski@yahoo.com>
2024-01-27 18:00:12 -0500

    change server

--- employeeFrontend/src/admin/affiliate/affiliate.service.ts
+++ employeeFrontend/src/admin/affiliate/affiliate.service.ts
@@ -7,19 +7,21 @@ import { AffiliateData } from './models/AffiliateData';
   providedIn: 'root'
 })
 export class AffiliateService {
-
+  //"http://localhost:8080"
+  //"https://shoppingtally.click/test/shoppingtally-0.0.2-SNAPSHOT"
+  serverUrl = "https://shoppingtally.click/test/shoppingtally-0.0.2-SNAPSHOT"
   constructor(private http:HttpClient) { }
 
   addAffiliateData(values):Observable<AffiliateData[]>{
-    return this.http.post<AffiliateData[]>("http://localhost:8080/api/v1/affiliate/add",values)
+    return this.http.post<AffiliateData[]>(this.serverUrl+"/api/v1/affiliate/add",values)
   }
 
   getAffiliateData():Observable<AffiliateData[]>{
-    return this.http.get<AffiliateData[]>("http://localhost:8080/api/v1/affiliate/get")
+    return this.http.get<AffiliateData[]>(this.serverUrl+"/api/v1/affiliate/get")
   }
 
   deleteAffiliateData(affiliateData:AffiliateData):Observable<AffiliateData[]>{
-    return this.http.post<AffiliateData[]>("http://localhost:8080/api/v1/affiliate/delete",affiliateData)
+    return this.http.post<AffiliateData[]>(this.serverUrl+"/api/v1/affiliate/delete",affiliateData)
 
   }
 }
