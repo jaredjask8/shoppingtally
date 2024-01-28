972578ed88bfa9015fb002e4eb7d6a9124112422
Jared <jaredjaskolski@yahoo.com>
2024-01-28 17:18:45 -0500

    add a guard to stop user from going to a link that needs account access

--- employeeFrontend/src/app/app-routing.module.ts
+++ employeeFrontend/src/app/app-routing.module.ts
@@ -1,21 +1,20 @@
 import { ApplicationConfig, NgModule, inject } from '@angular/core';
 import { RouterModule, Routes, provideRouter } from '@angular/router';
 import { adminGuard } from 'src/global/utility/admin.guard';
+import { loggedInGuard } from 'src/global/utility/logged-in.guard';
 import { HomeComponent } from 'src/home/home/home.component';
 import { RegisterComponent } from 'src/register/register.component';
 
 const routes: Routes = [
 	{path: 'home', component:HomeComponent, data: {animation:'homePage'}},
-	{path: 'register', component:RegisterComponent, data:{animation:'registerPage'}},
-	{ path: 'list', loadChildren: () => import('../list/list_component/list.module').then(m => m.ListModule) },
-	{ path: 'previousLists', loadChildren: () => import('../previousLists/previous-lists.module').then(m => m.PreviousListsModule) },
-	{ path: 'employee', loadChildren: () => import('../employee/employee.module').then(m => m.EmployeeModule) },
+	{ path: 'list', loadChildren: () => import('../list/list_component/list.module').then(m => m.ListModule),canActivate:[loggedInGuard] },
+	{ path: 'previousLists', loadChildren: () => import('../previousLists/previous-lists.module').then(m => m.PreviousListsModule),canActivate:[loggedInGuard] },
 	{ path: 'about', loadChildren: () => import('../about/about.module').then(m => m.AboutModule) },
-	{ path: 'profile', loadChildren: () => import('../profile/profile.module').then(m => m.ProfileModule) },
+	{ path: 'profile', loadChildren: () => import('../profile/profile.module').then(m => m.ProfileModule),canActivate:[loggedInGuard] },
 	{ path: 'reviews', loadChildren: () => import('../reviews/reviews.module').then(m => m.ReviewsModule) },
 	{ path: 'admin', loadChildren: () => import('../admin/admin.module').then(m => m.AdminModule), canActivate:[adminGuard]}, //, canActivate:[adminGuard]
-	{ path: 'mealkit', loadChildren: () => import('../mealkit/mealkit.module').then(m => m.MealkitModule) },
-	{ path: 'howItWorks', loadChildren: () => import('../how-it-works/how-it-works.module').then(m => m.HowItWorksModule) },
+	{ path: 'mealkit', loadChildren: () => import('../mealkit/mealkit.module').then(m => m.MealkitModule),canActivate:[loggedInGuard] },
+	{ path: 'howItWorks', loadChildren: () => import('../how-it-works/how-it-works.module').then(m => m.HowItWorksModule),canActivate:[loggedInGuard] },
 	{ path: '**', redirectTo: 'home' }
 ];
 
--- employeeFrontend/src/global/utility/admin.guard.ts
+++ employeeFrontend/src/global/utility/admin.guard.ts
@@ -1,11 +1,8 @@
 import { inject } from "@angular/core";
 import { CanActivateFn, Router } from "@angular/router";
-import { RegisterService } from "src/register/register.service";
 
 export const adminGuard:CanActivateFn = (route,state) => {
-    const registerService = inject(RegisterService);
     const router = inject(Router);
-    console.log(registerService.isAdmin)
     if(sessionStorage.getItem("admin")){
         return true;
     }else{
