bf3eb3e6935b23044c413863835c3d141738c6ef
Jared <jaredjaskolski@yahoo.com>
2024-02-05 23:37:37 -0500

    update socket url

--- employeeFrontend/src/environments/environment.development.ts
+++ employeeFrontend/src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    apiUrl:"https://shoppingtallydev.com/dev/shoppingtally-0.0.2-SNAPSHOT"
+    apiUrl:"https://shoppingtallydev.com/dev/shoppingtally-0.0.2-SNAPSHOT",
+    socketUrl:"https://shoppingtally.click/test/shoppingtally-0.0.2-SNAPSHOT"
 };
